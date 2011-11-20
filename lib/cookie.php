f8a925d77a4b0e27fbee519e7ab270bff94f1c69
Tai-Jin Lee <taijin@gmail.com>
2011-11-20 11:40:18 -0800

    fixing cookie default domain

--- lib/cookie.php
+++ lib/cookie.php
@@ -6,9 +6,10 @@ namespace lib;
  */
 class cookie {
 
-  const DEFAULT_COOKIE_DOMAIN = '.rezassured.com';
-
-  public static function set($name, $value, $expire = 0, $path = '/', $domain = self::DEFAULT_COOKIE_DOMAIN, $secure = false, $http_only = false) {
+  public static function set($name, $value, $expire = 0, $path = '/', $domain = false, $secure = false, $http_only = false) {
+    if (!$domain) {
+      $domain = \lib\conf\constants::$domain;
+    }
     setcookie($name, $value, $expire, $path, $domain, $secure, $http_only);
   }
 
